36dddaceed8e1153d75eb20e395ce16bac015c56
lei-hsia <GenuineLei@163.com>
2020-07-21 09:47:44 +0800

    BestTimetoBuySellstock once: update res,min while traversing array; BestTimetoBuySellstock2 infinite: greedy: find min/max in an interval, add to profit

---
+++ src/main/java/leetcode/BestTimetoBuySellstock1.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class BestTimetoBuySellstock1 {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        if (n == 0) return 0;
+        int res = 0, min = prices[0];
+        for (int i = 1; i < n; ++i) {
+            res = Math.max(res, prices[i] - min);
+            min = Math.min(min, prices[i]);
+        }
+        return res;
+    }
+}
---
+++ src/main/java/leetcode/BestTimetoBuySellstock2.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+/* greedy */
+public class BestTimetoBuySellstock2 {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int i = 0, buy = 0, sell = 0, profit = 0, n = prices.length - 1;
+        while (i < n) {
+
+            while (i < n && prices[i+1] <= prices[i]) ++i;
+            buy = prices[i];
+
+            while (i < n && prices[i+1] > prices[i]) ++i;
+            sell = prices[i];
+
+            profit += (sell - buy);
+        }
+
+        return profit;
+    }
+}
