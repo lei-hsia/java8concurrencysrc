e5a669389d8ccd6dad856a27844d63f7f93bee69
lei-hsia <GenuineLei@163.com>
2020-08-25 16:25:48 +0800

    JvmThread: threadA oom, threadB still running

---
+++ src/main/java/oom/JvmThread.java
@@ -0,0 +1,37 @@
+package oom;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+// unable to create new native thread; heapspace, stackoverflow, etc.
+public class JvmThread {
+    // https://zhuanlan.zhihu.com/p/151028855
+    public static void main(String[] args) {
+        new Thread(() -> {
+            List<byte[]> list = new ArrayList<byte[]>();
+            while (true) {
+                System.out.println(new Date().toString() + Thread.currentThread() + "==");
+                byte[] b = new byte[1024 * 1024 * 1];
+                list.add(b);
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        // 线程二
+        new Thread(() -> {
+            while (true) {
+                System.out.println(new Date().toString() + Thread.currentThread() + "==");
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+}
