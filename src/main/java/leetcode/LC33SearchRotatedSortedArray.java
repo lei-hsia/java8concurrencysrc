0de01a3d9920e41cc2eded5fd0b895f71b4bbff7
lei-hsia <GenuineLei@163.com>
2020-11-13 11:37:34 +0800

    left <= right

---
+++ src/main/java/leetcode/LC33SearchRotatedSortedArray.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class LC33SearchRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) { // 有nums[mid] == target: 所以while循环中有等于
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < nums[right]) {
+                if (nums[mid] < target && target <= nums[right]) left = mid+1;
+                else right = mid-1;
+            } else {
+                if (nums[mid] > target && target >= nums[left]) right = mid-1;
+                else left = mid+1;
+            }
+        }
+        return -1;
+    }
+}
