6a0e69a72bfe722502744be9c27e7bd9508e0394
lei-hsia <GenuineLei@163.com>
2020-11-18 21:32:31 +0800

    dada oa

--- src/main/java/dada/WeightedNum.java
+++ src/main/java/dada/WeightedNum.java
@@ -27,9 +27,9 @@ public class WeightedNum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int total = in.nextInt();
-        String[] strs = new String[total];
+        //String[] strs = new String[total];
         for (int i = 0; i < total; i++) {
-            strs[i] = in.nextLine();
+            //strs[i] = in.nextLine();
         }
     }
 }
