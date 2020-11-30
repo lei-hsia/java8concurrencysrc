accce2a04459aca115323d00d9f3b1846c3adabc
lei-hsia <GenuineLei@163.com>
2020-11-30 10:22:50 +0800

    TwoStackOneArray

--- src/main/java/leetcode/lc207CourseSchedule.java
+++ src/main/java/leetcode/lc207CourseSchedule.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -8,7 +9,6 @@ public class lc207CourseSchedule {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList<Integer>[] graph = new ArrayList[numCourses]; // 每个index表示从哪个出发，index的arraylist包含所有到达的
         int[] in = new int[numCourses]; // 每个顶点的入度
-
         for (int i = 0; i < graph.length; ++i) {
             graph[i] = new ArrayList();
         }
@@ -17,7 +17,6 @@ public class lc207CourseSchedule {
             graph[p[1]].add(p[0]);
             in[p[0]]++;
         }
-
         Queue<Integer> q = new LinkedList();
         // for (int i : in) {
         //     if (i == 0) q.offer(i);
---
+++ src/main/java/stack/TwoStackOneArray.java
@@ -0,0 +1,70 @@
+package stack;
+
+public class TwoStackOneArray {
+    int size;
+    int top1, top2;
+    int arr[];
+
+    public TwoStackOneArray(int n) {
+        arr = new int[n];
+        size = n;
+        top1 = -1;
+        top2 = size;
+    }
+
+    public void push1(int x) {
+        if (top1 < top2 - 1){
+            top1++;
+            arr[top1] = x;
+        }else {
+            System.out.println("stack overflow");
+            System.exit(1);
+        }
+    }
+
+    public void push2(int x) {
+        if (top1 < top2 - 1){
+            top2--;
+            arr[top2] = x;
+        }else {
+            System.out.println("stack overflow");
+            System.exit(1);
+        }
+    }
+
+    public int pop1(){
+        if (top1 >= 0){
+            int x = arr[top1];
+            top1--;
+            return x;
+        }else {
+            System.out.println("stack underflow");
+        }
+        return 0;
+    }
+
+    public int pop2(){
+        if (top2 < size){
+            int x = arr[top2];
+            top2++;
+            return x;
+        }else {
+            System.out.println("stack underflow");
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        TwoStackOneArray ts = new TwoStackOneArray(5);
+        ts.push1(5);
+        ts.push2(10);
+        ts.push2(15);
+        ts.push1(11);
+        ts.push2(7);
+        System.out.println("Popped element from"
+                + " stack1 is " + ts.pop1());
+        ts.push2(40);
+        System.out.println("Popped element from"
+                + " stack2 is " + ts.pop2());
+    }
+}
